351eefd26dcab2b25ffb4020863715bc91edfe64
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 17:52:45 -0500

    Update code to remove None check

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if str == None:
-        return
     for i in range(len(str)):
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
             atoA = ord(str[i]) + (ord('A') - ord('a'))
