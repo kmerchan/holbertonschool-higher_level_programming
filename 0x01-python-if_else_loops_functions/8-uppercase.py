6c0f0f55a7bf39c8c82e815f37566e9ab2d5d730
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 17:44:26 -0500

    Update code to include check if input string is NULL

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
+    if str == NULL:
+        return
     for i in range(len(str)):
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
             atoA = ord(str[i]) + (ord('A') - ord('a'))
