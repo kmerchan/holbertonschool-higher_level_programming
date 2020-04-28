bb0bfed69009854694583bdf503df4ee6c073d58
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 14:27:17 -0500

    Update code to handle non-zero last digits of negative numbers also as negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number > 0:
+    print("Last digit of {} is ".format(number), end='')
+    print("{} and is ".format(number % 10), end='')
+    if (number % 10) > 5:
+        print("greater than 5")
+    elif (number % 10) == 0:
+        print("0")
+    else:
+        print("less than 6 and not 0")
 if number < 0:
     number2 = 0 - number
-else:
-    number2 = number
-print("Last digit of {} is {} and is ".format(number, (number2 % 10)), end='')
-if (number2 % 10) > 5:
-    print("greater than 5")
-elif (number2 % 10) == 0:
-    print("0")
-else:
-    print("less than 6 and not 0")
+    print("Last digit of {} is ".format(number), end='')
+    if (number2 % 10) == 0:
+        print("{} and is 0". format(number2 % 10))
+    else:
+        print("-{} and is less than 6 and not 0". format(number2 % 10))
