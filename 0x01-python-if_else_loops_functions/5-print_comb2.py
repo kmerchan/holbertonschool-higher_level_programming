a14ae2f5613fc4ac5d438176aaf9cd4341c631ab
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 15:14:41 -0500

    Update code to reduce print statements with format or end conditions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
+for i in range(0, 99):
     if i < 10:
         print("0", end='')
-    if i != 99:
-        print("{}, ".format(i), end='')
-    else:
-        print("{}".format(i))
+    print("{}, ".format(i), end='')
+print("99")
