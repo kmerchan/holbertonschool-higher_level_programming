3088d36a22859567838590d4995a0231014b7006
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 14:34:35 -0500

    Update conditional check to include if number is 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
-    print("Last digit of {} is ".format(number), end='')
+print("Last digit of {} is ".format(number), end='')
+if number >= 0:
     print("{} and is ".format(number % 10), end='')
     if (number % 10) > 5:
         print("greater than 5")
@@ -10,9 +10,8 @@ if number > 0:
         print("0")
     else:
         print("less than 6 and not 0")
-if number < 0:
+elif number < 0:
     number2 = 0 - number
-    print("Last digit of {} is ".format(number), end='')
     if (number2 % 10) == 0:
         print("{} and is 0". format(number2 % 10))
     else:
