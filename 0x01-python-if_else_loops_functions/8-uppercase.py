6b6178700d615893a7dbe71380ebe3a8ff596f6d
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 16:57:41 -0500

    Add Python code to define function to print input string in all uppercase, followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            atoA = ord(str[i]) + (ord('A') - ord('a'))
+        else:
+            atoA = ord(str[i])
+        print("{}".format(chr(atoA)), end='')
+        if i == (len(str) - 1):
+            print("")
