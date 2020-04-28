6059fd2437156b7bfdf2ca0fc54b73a399062a01
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 17:51:24 -0500

    Update NULL check to check for None (Python)

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if str == NULL:
+    if str == None:
         return
     for i in range(len(str)):
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
