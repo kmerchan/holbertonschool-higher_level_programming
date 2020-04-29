d2575fd777939a28bc4ac6748498911d1f3a3553
Kelsie Merchant <eislek02@gmail.com>
2020-04-28 23:41:26 -0500

    Add C source code file to define function to insert new node in order in numerically sorted linked list and add header file with linked list struct definition, included libraries, and function prototypes

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+
+/**
+ * insert_node - function to insert new node in sorted linked list
+ * @head: input pointer to head pointer of singly linked list
+ * @number: input number to sort and insert into list as new node
+ * Return: pointer to newly added node or NULL if failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newnode, *turtle, *rabbit;
+
+	if (head == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = number;
+	newnode->next = NULL;
+	if (*head == NULL)
+	{
+		(*head) = newnode;
+		return (*head);
+	}
+	if ((*head)->n > number)
+	{
+		newnode->next = (*head);
+		(*head) = newnode;
+		return (*head);
+	}
+	turtle = (*head);
+	rabbit = (*head);
+	while (rabbit->next != NULL && rabbit->next->next != NULL)
+	{
+		rabbit = rabbit->next->next;
+		if (rabbit->n < number)
+			turtle = rabbit;
+		else if (turtle->next->n < number)
+		{
+			newnode->next = turtle->next->next;
+			turtle->next->next = newnode;
+			return (newnode);
+		}
+		else if (turtle->n < number)
+		{
+			newnode->next = turtle->next;
+			turtle->next = newnode;
+			return (newnode);
+		}
+	}
+	if (rabbit->next == NULL)
+	{
+		rabbit->next = newnode;
+		return (newnode);
+	}
+	if (rabbit->next->n > number)
+	{
+		rabbit->next->next = newnode;
+		return (newnode);
+	}
+	newnode->next = rabbit->next;
+	rabbit->next = newnode;
+	return (newnode);
+}
