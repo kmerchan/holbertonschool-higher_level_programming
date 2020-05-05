e167b8a7014acd5525e406aca6f556d41fcecc46
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 16:15:31 -0500

    Update Python function code to fix typo in variable name my_list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(mylist=[]):
-    for i in range(len(mylist)):
-        print("{:d}".format(mylist[i]))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
