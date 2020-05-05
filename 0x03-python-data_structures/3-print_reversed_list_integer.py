909ce2646e0423ad9edb0a7e3b8ae8d16bc39d97
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 18:51:53 -0500

    Update check for if passed an empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return
-    for i in reversed(range(len(my_list))):
-        print("{:d}".format(my_list[i]))
+    if my_list:
+        for i in reversed(range(len(my_list))):
+            print("{:d}".format(my_list[i]))
