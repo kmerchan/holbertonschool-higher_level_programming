afccccebb9923b95ce1d101f445e917ef1f611a9
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 16:45:03 -0500

    Update code to check for empty list and if list is only one element

--- 0x03-python-data_structures/test_files/3-main.py
+++ 0x03-python-data_structures/test_files/3-main.py
@@ -3,3 +3,7 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+my_list[:] = []
+print_reversed_list_integer(my_list)
+my_list = [20]
+print_reversed_list_integer(my_list)
