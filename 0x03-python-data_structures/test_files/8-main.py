23f29d48934b0296f36363f3a3e1cab8405ddc06
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 18:50:06 -0500

    Add Python program to test new function to return tuple with length of input string and first character of the string

---
+++ 0x03-python-data_structures/test_files/8-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+
+sentence = "At Holberton school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+length, first = multiple_returns("")
+print("Length: {:d} - First character: {}".format(length, first))
