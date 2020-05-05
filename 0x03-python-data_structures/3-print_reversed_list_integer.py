621e897ff0024681f976a8b1181d8487416e1a49
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 16:44:09 -0500

    Update code to only print if my_list is not empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
     for i in reversed(range(len(my_list))):
         print("{:d}".format(my_list[i]))
