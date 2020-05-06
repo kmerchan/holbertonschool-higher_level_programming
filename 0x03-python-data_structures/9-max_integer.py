94466789e128f7f2116d3034bc6e92172ce20e34
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 19:40:24 -0500

    Update code to start with first value of list instead of 0 to account for if list is all negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        new_list = [0]
+        new_list = [my_list[0]]
         for i in my_list:
             if i > new_list[0]:
                 new_list[0] = i
