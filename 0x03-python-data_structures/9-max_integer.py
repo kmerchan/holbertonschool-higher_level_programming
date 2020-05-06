ea6c741373e85814369c6d40f7e339ddc8bf6348
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 19:32:05 -0500

    Add Python function to find the biggest integer of a list or return None if the list is empty

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        new_list = [0]
+        for i in my_list:
+            if i > new_list[0]:
+                new_list[0] = i
+        return(new_list[0])
+    return(None)
