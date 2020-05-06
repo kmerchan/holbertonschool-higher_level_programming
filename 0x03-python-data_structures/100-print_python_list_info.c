2d4eec0d32aa638d462d7a491b28bb757d0af5e8
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 23:14:38 -0500

    Add C source code to utilize input PyObject pointer to find PyList_Size for length and PyList_Type to find the type of each element

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,3 +1,5 @@
+#include <Python.h>
+
 /**
  * print_python_list_info - function to print basic info about Python lists
  * @p: input pointer to PyObject or the Python list to print info about
@@ -5,5 +7,10 @@
 
 void print_python_list_info(PyObject *p)
 {
-	return;
+	ssize_t len = PyList_Size(p);
+
+	printf("[*] Size of the Python list = %ul\n", len);
+	printf("[*] Allocated = %d\n", len);
+	for (i = 0, i < len; i++)
+		printf("Element %d: %s\n", i, PyList_Type(p[i]));
 }
