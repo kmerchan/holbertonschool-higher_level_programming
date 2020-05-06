213f6d4c30862fdf91067fe43a267dbf2f091eec
Kelsie Merchant <eislek02@gmail.com>
2020-05-05 22:49:31 -0500

    Initial commit of C program file to print basic info about Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,9 @@
+/**
+ * print_python_list_info - function to print basic info about Python lists
+ * @p: input pointer to PyObject or the Python list to print info about
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	return;
+}
