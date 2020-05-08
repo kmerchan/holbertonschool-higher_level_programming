015584999acdb8306fe9a7323927782e6bdd485c
Kelsie Merchant <eislek02@gmail.com>
2020-05-07 22:18:17 -0500

    Add Python function to return new matrix with the squared values of given matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [ [], [], [] ]
+    row_index = 0
+    for row in matrix:
+        new_matrix[row_index] = [x**2 for x in row]
+        row_index += 1
+    return(new_matrix)
