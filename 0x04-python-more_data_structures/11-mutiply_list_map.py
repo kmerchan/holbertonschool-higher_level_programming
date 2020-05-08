add3a739ebc0b9e9379912e5650037e2cd66283b
Kelsie Merchant <eislek02@gmail.com>
2020-05-07 21:40:22 -0500

    Update Python code to more efficiently multiply all values of my_list by number

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    num_list = [number] * len(my_list)
-    new_list = list(map(lambda x, y: x * y, my_list, num_list))
+    new_list = list(map(lambda x: x * number, my_list))
     return(new_list)
