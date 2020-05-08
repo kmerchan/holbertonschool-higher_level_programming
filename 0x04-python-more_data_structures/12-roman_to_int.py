6e503e2517f86a5d5002ebd9eda6153f8763cc33
Kelsie Merchant <eislek02@gmail.com>
2020-05-08 01:17:15 -0500

    Update code to check if input roman_string is a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     if not(roman_string):
         return(0)
+    if type(roman_string) != str:
+        return(0)
     value = 0
     convert = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for i in range(len(roman_string)):
