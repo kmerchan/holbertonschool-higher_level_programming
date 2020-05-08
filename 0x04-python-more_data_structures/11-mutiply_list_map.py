db4ab3f727638d0fe5d8b266bd77821f90e893d4
Kelsie Merchant <eislek02@gmail.com>
2020-05-07 20:46:35 -0500

    Update method for setting num_list to be list of length my_list with all values number without using loop

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    num_list = my_list[:]
-    for i in range(len(num_list)):
-        num_list[i] = number
+    num_list = [number] * len(my_list)
     new_list = list(map(lambda x, y: x * y, my_list, num_list))
     return(new_list)
