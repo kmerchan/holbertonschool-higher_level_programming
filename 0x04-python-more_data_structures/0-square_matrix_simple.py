7c3b782e4366ee07911aa7f51886fe155801f09e
Kelsie Merchant <eislek02@gmail.com>
2020-05-07 22:28:47 -0500

    Update code to start with empty matrix with the same number of rows as matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [ [], [], [] ]
+    new_matrix = [[1]] * len(matrix)
     row_index = 0
     for row in matrix:
         new_matrix[row_index] = [x**2 for x in row]
