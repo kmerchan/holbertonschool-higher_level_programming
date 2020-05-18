f9c6c08fb850f12156991668edbea5e16672214a
Kelsie Merchant <eislek02@gmail.com>
2020-05-18 13:17:49 -0500

    Update code to include finally: statement with supported code with no errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,8 +2,9 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [0]*list_length
     for i in range(list_length):
+        quotient = 0
         try:
-            new_list[i] = (my_list_1[i] / my_list_2[i])
+            quotient = (my_list_1[i] / my_list_2[i])
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
@@ -11,5 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            continue
+            new_list[i] = quotient
     return new_list
