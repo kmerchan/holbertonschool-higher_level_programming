6d76aa879fcc9c665678e59065b494a92e35192a
Kelsie Merchant <eislek02@gmail.com>
2020-05-18 16:06:10 -0500

    Update except to catch all possible exceptions caused by the function, not just a select specific ones

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,6 @@ def safe_function(fct, *args):
     import sys
     try:
         return (fct(*args))
-    except (IndexError, TypeError, ValueError,
-            ZeroDivisionError, NameError) as error:
+    except Exception as error:
         sys.stderr.write("Exception: {}\n".format(error))
         return (None)
