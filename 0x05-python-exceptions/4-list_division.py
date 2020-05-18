fb9e757cdf986f847bf26405c190145c8f5dbb2e
Kelsie Merchant <eislek02@gmail.com>
2020-05-18 13:14:11 -0500

    Update function format to include finally:

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,4 +10,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except IndexError:
             print("out of range")
+        finally:
+            continue
     return new_list
