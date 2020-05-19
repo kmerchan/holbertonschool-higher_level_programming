27f391c14bdb4de60131268623ad6f81420186f3
Kelsie Merchant <eislek02@gmail.com>
2020-05-18 19:33:29 -0500

    Initial commit of C functions to print information about Python lists, bytes, and float from PyObject

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,33 @@
+#import <stdio.h>
+#import <stdlib.h>
+#import <Python.h>
+
+/**
+ * print_python_list - function to print information about Python lists
+ * @p: PyObject to get information from
+ */
+
+void print_python_list(PyObject *p)
+{
+	(void)p;
+}
+
+/**
+ * print_python_bytes - function to print information about Python bytes
+ * @p: PyObject to get information from
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	(void)p;
+}
+
+/**
+ * print_python_float - function to print information about Python floats
+ * @p: PyObject to get information from
+ */
+
+void print_python_float(PyObject *p)
+{
+	(void)p;
+}
