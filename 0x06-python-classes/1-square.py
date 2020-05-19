4219f51652cc5ad7c87094a1ae9d2b46c7a050ce
Kelsie Merchant <eislek02@gmail.com>
2020-05-19 16:44:52 -0500

    Add Python class definition Square with private instance attribute size instantiated to size with no validation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creates empty class Square"""
+
+
+class Square:
+    """defines class and instantiates private instance attribute size."""
+
+    def __init__(self, size):
+        self.__size = size
