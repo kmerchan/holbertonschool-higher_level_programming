cf9a9bbeb584bbcfbc4418b08ffa7c243b4be0bd
Kelsie Merchant <eislek02@gmail.com>
2020-05-19 21:32:33 -0500

    Add Python Square class definition with new method to print square like a string and to return representation of square so print statement will print the square like my_print method

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""creates class Square with
+private instance attribute size and position and
+public instance methods to calculate area and print square"""
+
+
+class Square:
+    """defines class with private instance attributes size and position
+and public instance methods to calculate area and print square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return(self.__position)
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        """prints square of size self.__size using #"""
+        if self.__size > 0:
+            for y in range(self.__position[1]):
+                print()
+            for row in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
+
+    def my_print_string(self):
+        """formats returnable string like my_print"""
+        square_string = ""
+        if self.__size > 0:
+            for y in range(self.__position[1]):
+                square_string = square_string + "\n"
+            for row in range(self.__size):
+                for x in range(self.__position[0]):
+                    square_string = square_string + " "
+                for column in range(self.__size):
+                    square_string = square_string + "#"
+                if row is not (self.__size - 1):
+                    square_string = square_string + "\n"
+        else:
+            square_string = "\n"
+        return square_string
+
+    def __repr__(self):
+        return (self.my_print_string())
