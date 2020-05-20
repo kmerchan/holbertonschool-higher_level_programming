a8e899f2a6e239fc40d36b80fbfcdae73fcfc2d5
Kelsie Merchant <eislek02@gmail.com>
2020-05-20 14:59:22 -0500

    Update Python code to define printable singly linked list class that can add nodes in order and define node class to create new node instances

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -30,7 +30,7 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """setter for next_node attribute"""
-        if type(value) is not None:
+        if value != None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
@@ -40,7 +40,39 @@ class SinglyLinkedList:
     """defines class for singly linked list"""
 
     def __init__(self):
-        SinglyLinkedList.__head = None
+        self.__head = None
 
     def sorted_insert(self, value):
-        pass
+        """insert new node"""
+        new_node = Node(value)
+        if self.__head is None:
+            self.__head = new_node
+        else:
+            tmp = self.__head
+            if tmp.data > new_node.data:
+                new_node.next_node = tmp
+                self.__head = new_node
+                return
+            while tmp.next_node is not None:
+                tmp2 = tmp.next_node
+                if tmp2.data < new_node.data:
+                    tmp = tmp2
+                else:
+                    new_node.next_node = tmp.next_node
+                    tmp.next_node = new_node
+                    return
+            tmp.next_node = new_node
+
+    def stringrep(self):
+        strrep = ""
+        tmp = self.__head
+        while tmp is not None:
+            datavalue = tmp.data
+            strrep = strrep + str(datavalue)
+            tmp = tmp.next_node
+            if tmp:
+                strrep = strrep + "\n"
+        return strrep
+
+    def __repr__(self):
+        return(self.stringrep())
