529b0e9a4be71ab202753d7fb08ee66ca309fe3f
Kelsie Merchant <eislek02@gmail.com>
2020-05-19 20:28:27 -0500

    Update property for position to check if each values stored in the tuple to set position are int types

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,9 @@ and public instance method."""
     def position(self, value):
         if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
+        elif type(value[0]) is not int or type(value[2]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
