2356a0e2c7fecc5108598aa6d7dab8c56c5d1ff3
Kelsie Merchant <eislek02@gmail.com>
2020-05-19 20:07:50 -0500

    Add Python code to define class Square and add additional public instance method to print the square of dimensions given by the private instance attribute size using the '#' character

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""creates class Square with
+private instance attribute size and public instance method"""
+
+
+class Square:
+    """defines class with instantiated and validated private instance attribute
+and public instance method."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        """prints square of size self.__size using #"""
+        if self.__size > 0:
+            for column in range(self.__size):
+                for row in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
