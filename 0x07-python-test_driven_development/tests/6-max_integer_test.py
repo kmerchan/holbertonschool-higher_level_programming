21ab67272091b701b58c6d7cddf45f3a85f397dc
Kelsie Merchant <eislek02@gmail.com>
2020-05-22 22:50:02 -0500

    Update code to follow pep8 style guidelines

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+    """contains test functions for max_integer function"""
+
     def test_area(self):
+        """tests successful cases of max_integer"""
         self.assertEqual(max_integer([1, 2]), 2)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([1]), 1)
@@ -13,7 +18,9 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, -2, -3]), -1)
 
     def test_errors(self):
+        """tests errors raised by incorrect type arguments"""
         self.assertRaises(Exception, max_integer, ["string", 1.73, 25, {2}])
 
     def test_empty(self):
+        """tests errors if argument is none"""
         self.assertIsNone(max_integer())
