c417afceda3e95b61b1bfe9eec2f71cc16ac791f
Kelsie Merchant <eislek02@gmail.com>
2020-06-02 19:21:09 -0500

    Add Python function to determine if object is an instance of a subclass of given class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines function to see if object is an instance of a class
+that is a subclass of the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if object is an instance of a subclass of the given class"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
