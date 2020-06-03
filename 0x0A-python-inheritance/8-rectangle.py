825722de21d9053db7cb58278b255e2acecf08ac
Kelsie Merchant <eislek02@gmail.com>
2020-06-02 22:56:29 -0500

    Add Python class Rectangle that inherits from BaseGeometry and uses integer_validator method

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines class Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """class for rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
