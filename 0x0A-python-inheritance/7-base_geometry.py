f20b3306b8860ec4a1ace017729c42e42e303e14
Kelsie Merchant <eislek02@gmail.com>
2020-06-02 23:52:13 -0500

    Update to follow style guidelines

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,6 +9,7 @@ class BaseGeometry:
     def area(self):
         """raises exception that area method is not implemented"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """validates integer"""
         if type(value) is not int:
