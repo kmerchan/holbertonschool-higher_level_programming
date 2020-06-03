5450c97ef69e6edbf1a4bfa61a0e3ece7298b665
Kelsie Merchant <eislek02@gmail.com>
2020-06-02 21:43:10 -0500

    Add function to define class with public instance method area() to raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines class BaseGeomtry with public class method for area"""
+
+
+class BaseGeometry:
+    """class with public instance method to raise exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
