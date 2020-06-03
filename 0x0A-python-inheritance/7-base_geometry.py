c2371e1a353dc707ad7a0c1520fec60cd3649a1d
Kelsie Merchant <eislek02@gmail.com>
2020-06-02 22:03:25 -0500

    Add class definition with public instances for area() and integer validation

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines class BaseGeomtry
+with public instance methods for area and integer validation"""
+
+
+class BaseGeometry:
+    """class with public instance method to raise exception
+    and public instance method to validate integer"""
+    def area(self):
+        """raises exception that area method is not implemented"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """validates integer"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
