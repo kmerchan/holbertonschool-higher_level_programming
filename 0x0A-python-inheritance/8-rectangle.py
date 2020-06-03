035954761ab13c60364c24a3a0efabb1808cee30
Kelsie Merchant <eislek02@gmail.com>
2020-06-02 23:20:10 -0500

    Update class init to correctly set width and height attributes

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,5 +8,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """class for rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
