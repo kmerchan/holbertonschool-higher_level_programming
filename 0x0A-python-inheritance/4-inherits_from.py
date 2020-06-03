0468d0f0fbdbb10ed1e68470d2a2b9065605ecf5
Kelsie Merchant <eislek02@gmail.com>
2020-06-02 19:41:57 -0500

    Update function to follow style guidelines

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,8 @@ that is a subclass of the specified class"""
 
 
 def inherits_from(obj, a_class):
-    """returns True if object is an instance of a subclass of the given class"""
+    """returns True if object is an instance of a subclass
+    of the given class"""
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
