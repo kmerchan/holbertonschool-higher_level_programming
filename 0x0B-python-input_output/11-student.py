f2cffbf88ca4afa6e041b7cfd1e1f945f9c51819
Kelsie Merchant <eislek02@gmail.com>
2020-06-03 22:55:03 -0500

    Add Python class Student with public attributes and method to retrieve dictionary representation

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student():
+    """class to define student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes student instances"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns dictionary descript for JSON serialization"""
+        return obj.__dict__
