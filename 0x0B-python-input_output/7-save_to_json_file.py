765b6a86f5d8586f5495317f7665611bfb32f04b
Kelsie Merchant <eislek02@gmail.com>
2020-06-03 23:26:33 -0500

    Update to directly call json dump

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -5,6 +5,5 @@
 def save_to_json_file(my_obj, filename):
     """write JSON representation of an object to a file"""
     import json
-    j_str = json.dumps(my_obj, ensure_ascii=False)
     with open(filename, 'w') as f:
-        f.write(j_str)
+        f.write(json.dumps(my_obj, ensure_ascii=False))
