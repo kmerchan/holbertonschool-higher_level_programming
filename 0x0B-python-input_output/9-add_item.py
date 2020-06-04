dc20d9d8c245ec197bf6a9ce264da07c84aee51d
Kelsie Merchant <eislek02@gmail.com>
2020-06-03 23:44:04 -0500

    Update file name to be a string

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,9 +8,9 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 try:
-    arglist = load_from_json_file(add_item.json)
+    arglist = load_from_json_file("add_item.json")
 except FileNotFoundError:
     arglist = []
 
 arglist += argv[1:]
-save_to_json_file(arglist, add_item.json)
+save_to_json_file(arglist, "add_item.json")
