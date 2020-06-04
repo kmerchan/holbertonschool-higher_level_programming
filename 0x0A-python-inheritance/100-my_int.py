be076f6a5fa57544aa9b4c7dfcc6d9a4d22b3f46
Kelsie Merchant <eislek02@gmail.com>
2020-06-04 11:03:19 -0500

    Add Python class MyInt that inherits from int and switches equal and not equal function calls

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines rebel integer class"""
+
+class MyInt(int):
+    """rebel integer class that switches == and !="""
+    def __eq__(self, other):
+        """when equal is called, return not equal"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """when not equal is called, return equal"""
+        return super().__eq__(other)
