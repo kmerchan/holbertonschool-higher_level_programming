81560cc4f49bdb24d923568576882302fda967e2
Kelsie Merchant <eislek02@gmail.com>
2020-06-05 22:57:45 -0500

    Add Python function to print Pascal's triangle up to n rows

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -4,4 +4,22 @@
 
 def pascal_triangle(n):
     """returns list representing Pascal's triangle"""
-    return list[n]
+    pascal_triangle = []
+    previous = [1]
+    if n <= 0:
+        return pascal_triangle
+    for row_number in range(n):
+        rowlist = []
+        if row_number == 0:
+            rowlist = [1]
+        else:
+            for i in range(row_number + 1):
+                if i == 0:
+                    rowlist.append(0 + previous[i])
+                elif i == (row_number):
+                    rowlist.append(previous[i - 1] + 0)
+                else:
+                    rowlist.append(previous[i - 1] + previous[i])
+        previous = rowlist
+        pascal_triangle.append(rowlist)
+    return pascal_triangle
