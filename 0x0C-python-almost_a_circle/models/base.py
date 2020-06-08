3b2f135e5a8e3b5678894eb24867711346b11e0c
Kelsie Merchant <eislek02@gmail.com>
2020-06-08 11:13:08 -0500

    Add base class to keep track of ids for future classes and add __init__.py to make the models folder become a Python module

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines Base class"""
+
+
+class Base():
+    """manages id attribute in all future classes"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """initializes to given id or
+        increases class nb_objects and sets as default id"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
