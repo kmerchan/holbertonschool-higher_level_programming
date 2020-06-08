7600a604aa7e7f73f91795ca1e4061fb1369c9a0
Kelsie Merchant <eislek02@gmail.com>
2020-06-08 15:12:57 -0500

    Update Rectangle class to validate attributes in setter functions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,7 +23,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """sets private instance attribute width"""
-        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -33,7 +38,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """sets private instance attribute height"""
-        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -43,7 +53,12 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """sets private instance attribute x"""
-        self.__x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -53,4 +68,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """sets private instance attribute y"""
-        self.__y = value
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
