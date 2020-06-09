002e1d4e129635f2da1598b3f476b7f769376a3d
Kelsie Merchant <eislek02@gmail.com>
2020-06-08 21:33:32 -0500

    Update Square class to include getter/setter for size attribute

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,25 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initializes Square instance"""
         super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """gets private instance attribute size from width"""
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        """sets private instance attribute size and
+        uses to set width and height"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
+            self.__size = value
 
 
     def __str__(self):
