a1de1911efe7d125ff6f2f4cb779d88b305c8555
Kelsie Merchant <eislek02@gmail.com>
2020-06-08 20:15:59 -0500

    Add Square class that inherits from Rectangle class and uses Rectangle class attributes where width and height are both size

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defines Square class that inherits from Rectangle"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class for Square instances with
+    attributes from Rectangle class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes Square instance"""
+        super().__init__(size, size, x, y, id)
+
+
+    def __str__(self):
+        """override __str__ with new string in the format
+        [Square] (<id>) <x>/<y> - <size>"""
+        str_rep = "[Square] ({}) {}/{} - {}".format(str(self.id),
+        str(self.x), str(self.y), str(self.width))
+        return (str_rep)
