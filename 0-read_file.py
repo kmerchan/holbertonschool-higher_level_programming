6eb28461ed8bc125727c0257647865f04668e8b3
Kelsie Merchant <59209125+kmerchan@users.noreply.github.com>
2020-07-13 16:32:32 -0500

    Delete 0-read_file.py

--- 0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""defines function to read text file and prints to stdout"""
-
-
-def read_file(filename=""):
-    """reads text file and prints to stdout"""
-    f = open(filename, 'r')
-    print(f.read())
