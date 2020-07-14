2fe1a0b239a3b9a31f0f747ac97388205be02325
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 03:41:19 +0000

    Add script to create second_table in current database and add multiple rows of values

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- creates table second_table if does not exist and adds multiple rows
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
