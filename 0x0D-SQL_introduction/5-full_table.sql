125f9a4541baea2323f18717badaca707f2cf6f7
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 02:44:43 +0000

    Add script that prints the full description of newly created first_table in MySQL server

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints full description of first_table from hbtn_0c_0 database in MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+SHOW CREATE TABLE first_table;
