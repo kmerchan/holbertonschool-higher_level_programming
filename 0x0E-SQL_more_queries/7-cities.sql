b8ac3d98fe79d0adbbe494a32ee780cba764d7dc
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 22:12:50 -0500

    Update scripts to include auto increment before primary keys so they will be auto generated

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- table has id, state_id, and name
 -- id is primary key, state_id is foreign key, and name can't be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT PRIMARY KEY, state_id INT FOREIGN KEY REFERENCES state(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT FOREIGN KEY REFERENCES state(id), name VARCHAR(256) NOT NULL);
