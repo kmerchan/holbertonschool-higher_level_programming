4b45130d13da2dbe1d3b39043b9fc845eff58b81
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 13:49:10 -0500

    Remove backticks

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 -- lists all genres of tv show 'Dexter'
 -- display as tv_genres.name
-SELECT g.`name`
-       FROM `tv__show_genres` AS tvg
-       INNER JOIN `tv_shows` AS tv
-       ON tvg.`show_id` = tv.`id`
-       INNER JOIN `tv_genres` AS g
-       ON g.`id` = tvg.`genre_id`
-       WHERE tv.`title` = "Dexter"
-       ORDER BY g.`name` ASC;
+SELECT g.name
+       FROM tv_show_genres AS tvg
+       INNER JOIN tv_shows AS tv
+       ON tvg.show_id = tv.id
+       INNER JOIN tv_genres AS g
+       ON g.id = tvg.genre_id
+       WHERE tv.title = "Dexter"
+       ORDER BY g.name ASC;
