393d39ae937435cd2a0d441150b3caee3a80db31
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 14:28:25 -0500

    Update script to use LEFT JOIN to include TV shows without linked genre name

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,8 +2,8 @@
 -- display as tv_shows.title - tv_genres.name
 SELECT tv.title, g.name
        FROM tv_show_genres AS tvg
-       INNER JOIN tv_shows AS tv
+       LEFT JOIN tv_shows AS tv
        ON tvg.show_id = tv.id
-       INNER JOIN tv_genres AS g
+       LEFT JOIN tv_genres AS g
        ON g.id = tvg.genre_id
        ORDER BY tv.title, g.name ASC;
