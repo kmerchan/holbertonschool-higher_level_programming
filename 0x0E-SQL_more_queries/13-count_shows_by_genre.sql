6a21da3b0d5bc7331798d90e79ee2e573b57a437
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 13:17:57 -0500

    Update script to group by same table reference as first table

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ SELECT g.name, COUNT(t.show_id) AS number_of_shows
        FROM tv_genres AS g
        LEFT JOIN tv_show_genres AS t
        ON g.id = t.genre_id
-       GROUP BY t.genre_id
+       GROUP BY g.id
        ORDER BY number_of_shows DESC;
