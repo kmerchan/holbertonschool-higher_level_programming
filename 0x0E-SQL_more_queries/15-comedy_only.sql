2b8da665611c3545fa4be33c8edccc82794b6b47
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 14:19:26 -0500

    Update script to include tv shows with unlinked genre names

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,9 +2,9 @@
 -- display as tv_shows.title
 SELECT tv.title
        FROM tv_show_genres AS tvg
-       INNER JOIN tv_shows AS tv
+       LEFT JOIN tv_shows AS tv
        ON tvg.show_id = tv.id
-       INNER JOIN tv_genres AS g
+       LEFT JOIN tv_genres AS g
        ON g.id = tvg.genre_id
        WHERE g.name = "Comedy"
        ORDER BY tv.title ASC;
