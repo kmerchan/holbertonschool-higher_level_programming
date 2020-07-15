212a826172b55bfdb047c7e5dc170cafe7d6f514
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 02:05:57 +0000

    Update scripts after testing locally in container and fixing bugs

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates MySQL server user user_0d_1 with all privileges
 -- user_0d_1 should hava password user_0d_1_pwd
-CREATE USER user_0d_1 IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1;
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
