ae67e29bd560c47debe8b28a6ccf9cfb19caaf22
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 23:59:14 -0500

    Fix typo in table name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,3 @@
 -- lists all genres of tv show 'Dexter'
 -- display as tv_genres.name
-SELECT g.`name` FROM `tv_genres` AS g INNER JOIN `tv_shows_genre` AS tvg ON g.`id` = tvg.`genre_id` INNER JOIN `tv_shows` AS tv ON tvg.`show_id` = tv.`id` ORDER BY g.`name` ASC;
+SELECT g.`name` FROM `tv_genres` AS g INNER JOIN `tv_show_genre` AS tvg ON g.`id` = tvg.`genre_id` INNER JOIN `tv_shows` AS tv ON tvg.`show_id` = tv.`id` ORDER BY g.`name` ASC;
