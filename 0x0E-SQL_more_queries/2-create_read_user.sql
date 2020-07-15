0353a23f3817cb20f852bd211a7ce17cd805a9b1
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 02:22:44 +0000

    Remove miscellaneous extra character

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
-B-- creates database hbtn_0d_2 and user user_0d_2
+-- creates database hbtn_0d_2 and user user_0d_2
 -- user_0d_2 should hava password user_0d_2_pwd and only SELECT privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
