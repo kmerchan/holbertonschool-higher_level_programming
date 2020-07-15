f080a01c50666f28727a619c7bc0d285b1f82b74
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 02:20:47 +0000

    Fix spelling errors

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of MySQL users user_0d_1 and user_0d_2
 -- privileges are listed on localhost server
 SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRNATS FOR user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
--- creates database hbtn_0d_2 and user user_0d_2
+B-- creates database hbtn_0d_2 and user user_0d_2
 -- user_0d_2 should hava password user_0d_2_pwd and only SELECT privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbnt_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
