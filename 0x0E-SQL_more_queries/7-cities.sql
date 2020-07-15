6bb746d58b18fbb94f87dcd618316772abf36e25
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 22:37:53 -0500

    Update script to follow MySQL syntax instead of SQL Server syntax

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- table has id, state_id, and name
 -- id is primary key, state_id is foreign key, and name can't be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT FOREIGN KEY REFERENCES hbtn_0d_usa.state(id) NOT NULL, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id));
