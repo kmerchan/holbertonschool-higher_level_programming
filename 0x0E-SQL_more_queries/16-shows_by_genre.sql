0a23bbb91c0b2557cc6ffe0890c2dda7649e99aa
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 14:17:33 -0500

    Add script to list all tv show titles with their linked genre names

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- lists all TV show titles and linked genre names
+-- display as tv_shows.title - tv_genres.name
+SELECT tv.title, g.name
+       FROM tv_show_genres AS tvg
+       INNER JOIN tv_shows AS tv
+       ON tvg.show_id = tv.id
+       INNER JOIN tv_genres AS g
+       ON g.id = tvg.genre_id
+       ORDER BY tv.title, g.name ASC;
