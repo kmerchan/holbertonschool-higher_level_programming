59721f79f9a40136b0f90781f00e128b7cf5f101
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 22:46:46 -0500

    Remove line of comments

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,4 @@
--- creates database hbtn_0d_usa and table cities
--- table has id, state_id, and name
+-- creates database hbtn_0d_usa and table cities with id, state_id, and name
 -- id is primary key, state_id is foreign key, and name can't be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id));
