da49367c20acb085756b9908f5b2b08ce4398678
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 23:07:55 -0500

    Add script to use subquery to list all cities in California in current database, using SELECT statement to first get state_id for California and then to SELECT all cities with that state_id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,3 @@
 -- lists all the cities of California in current database
 -- results sorted by cities.id in ascending order
+SELECT `id`, `name` FROM `cities` WHERE `state_id` = (SELECT `id` FROM `states` WHERE `name` = "California") ORDER BY `id` ASC;
