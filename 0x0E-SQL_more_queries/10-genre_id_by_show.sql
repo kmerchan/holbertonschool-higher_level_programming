6824105aa8a0b4ab1d3c658a0b5616273928ba35
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 23:34:05 -0500

    Fix typo in table name

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- lists all shows in current database that have at least one genre linked
 -- display as tv_shows.title-tv_shows_genres.genre_id
-SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s INNER JOIN `tv_shows_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id` ASC;
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id` ASC;
