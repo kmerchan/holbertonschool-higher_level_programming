748b08deefc46cba2436bf8d7b415703ec101d1a
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 14:33:52 -0500

    Update script to flip first join statement to include tv shows as left most table

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- lists all TV show titles and linked genre names
 -- display as tv_shows.title - tv_genres.name
 SELECT tv.title, g.name
-       FROM tv_show_genres AS tvg
-       LEFT JOIN tv_shows AS tv
+       FROM tv_shows AS tv
+       LEFT JOIN tv_show_genres AS tvg
        ON tvg.show_id = tv.id
        LEFT JOIN tv_genres AS g
        ON g.id = tvg.genre_id
