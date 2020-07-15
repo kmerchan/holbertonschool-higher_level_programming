0d4a71abbd429edc94df2dc1610dac612dfb3f07
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 23:18:46 -0500

    Fix JOIN type to INNER JOIN and add AS to define table names used in statement

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- lists all the cities in current database, sorted by cities.id in ascending order
 -- displayed as [cities.id]-[cities.name]-[state.name]
-SELECT c.`id`, c.`name`, s.`name` FROM `cities` c FULL OUTER JOIN `states` s ON c.`state_id` = s.`id` ORDERED BY c.`id` ASC;
+SELECT c.`id`, c.`name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON c.`state_id` = s.`id` ORDERED BY c.`id` ASC;
