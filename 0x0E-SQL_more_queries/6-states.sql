f60cb188444a4794009168410b1b30f0fa811ad2
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 22:02:38 -0500

    Initial commit of scripts working with primary and foreign keys; currently untested, will be tested in container after pulled after merging this commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- creates database hbtn_0d_usa and table states
+-- table has id and name, where id is primary key and name can't be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
