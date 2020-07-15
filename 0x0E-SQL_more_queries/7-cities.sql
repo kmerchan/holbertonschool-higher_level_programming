8b17b0226a1c2ad13259a6d64d0367c16cff46d4
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 22:50:17 -0500

    Add 's' to table name for reference for foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,8 @@
 -- id is primary key, state_id is foreign key, and name can't be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
-FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
+FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
