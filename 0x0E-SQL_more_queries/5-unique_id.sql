21b98f4713f5906d65a9cfd4d58e93c304ac65b6
Kelsie Merchant <eislek02@gmail.com>
2020-07-14 21:48:11 -0500

    Update script to change how unique is applied to keys for correct syntax

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates table unique_id on MySQL server
 -- table has id and name, where id must be unique
-CREATE TABLE IF NOT EXISTS unique_id (UNIQUE KEY(id INT DEFAULT 1), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
