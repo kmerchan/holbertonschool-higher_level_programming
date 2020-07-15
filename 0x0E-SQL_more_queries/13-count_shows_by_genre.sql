845a19d4d350541b72eea0da23c77a771e2f50af
Kelsie Merchant <eislek02@gmail.com>
2020-07-15 10:17:53 -0500

    Update script to remove backticks and use JOIN

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- lists all genres and count of number of shows linked to that genre
 -- display as tv_genres.name-count of tv shows linked to that genre
-SELECT g.`name`, COUNT(t.`show_id`) AS number_of_shows
-       FROM `tv_genres` AS g
-       LEFT JOIN `tv_show_genres` AS t
-       ON g.`id` = t.`genre_id`
-       GROUP BY t.`genre_id`
+SELECT g.name, COUNT(t.show_id) AS number_of_shows
+       FROM tv_genres AS g
+       JOIN tv_show_genres AS t
+       ON g.id = t.genre_id
+       GROUP BY t.genre_id
        ORDER BY number_of_shows DESC;
