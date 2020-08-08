c2d47a499a03d09c81af91bc9808765be84191b6
Kelsie Merchant <eislek02@gmail.com>
2020-08-08 16:42:47 -0500

    Use LIKE BINARY to better match states to correct filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT id, name FROM states WHERE name REGEXP '^N'
+    cur.execute("""SELECT id, name FROM states WHERE name LIKE BINARY 'N%'
                 ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT id, name FROM states WHERE name = '{}' \
+    cur.execute("""SELECT id, name FROM states WHERE name LIKE BINARY '{}' \
                 ORDER BY states.id ASC""".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
