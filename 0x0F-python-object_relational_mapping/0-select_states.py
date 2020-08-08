05fecfa3a7d12c4094ca4960fd86d9cec9d0006b
Kelsie Merchant <eislek02@gmail.com>
2020-08-08 20:33:02 +0000

    Add script to use MySQLdb to list all states in database in ascending order by id; tested locally

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,12 +4,12 @@ lists all states from the database in ascending order
 using MySQLdb
 """
 if __name__ == "__main__":
-    import argv from sys
+    from sys import argv
     import MySQLdb
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM state ORDER BY states.id ASC")
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
