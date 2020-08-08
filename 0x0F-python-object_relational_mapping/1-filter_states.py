a05dd804e9181df0c7d57446dbc5c1d1396c94b1
Kelsie Merchant <eislek02@gmail.com>
2020-08-08 20:39:21 +0000

    Add script to list all states from a database with names starting with 'N' in order by id using MySQLdb; tested locally

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,13 +4,13 @@ lists all states from the database in ascending order
 where name of state starts with 'N' using MySQLdb
 """
 if __name__ == "__main__":
-    import argv from sys
+    from sys import argv
     import MySQLdb
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM state WHERE name REGEXP '^N'
-                ORDER BY states.id ASC")
+    cur.execute("""SELECT id, name FROM states WHERE name REGEXP '^N'
+                ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
