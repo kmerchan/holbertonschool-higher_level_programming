4453cdeeef271fa281f16a7d09a667568bf2cb8d
Kelsie Merchant <eislek02@gmail.com>
2020-08-08 21:14:12 +0000

    Add script to list all cities in ascending order by id, with their corresponding state name, using MySQLdb; tested locally

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,14 +4,14 @@ lists all cities from the database in ascending order
 with state name using MySQLdb
 """
 if __name__ == "__main__":
-    import argv from sys
+    from sys import argv
     import MySQLdb
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT c.id, c.name, s.name FROM city AS c
-                INNER JOIN states AS s ON c.state_id = s.id
-                ORDER BY c.id ASC")
+    cur.execute("""SELECT c.id, c.name, s.name FROM cities AS c \
+                INNER JOIN states AS s ON c.state_id = s.id \
+                ORDER BY c.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
