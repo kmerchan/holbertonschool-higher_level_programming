938cf11d8d4db93fc087bc747478c3d1cbd92ef6
Kelsie Merchant <eislek02@gmail.com>
2020-08-08 16:49:04 -0500

    Add script to safely return all states matching argument passed to script using MySQLdb

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT id, name FROM states WHERE name = '{}' \
-                ORDER BY states.id ASC""".format(argv[4]))
+    cur.execute("""SELECT id, name FROM states WHERE name = %(name)s \
+                ORDER BY states.id ASC""", {'name': argv[4]})
     rows = cur.fetchall()
     for row in rows:
         print(row)
