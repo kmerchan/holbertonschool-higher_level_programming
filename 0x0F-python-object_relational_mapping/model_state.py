2d8faae3981f402ef53a92bceb79d5ee3ed5a2be
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 12:06:13 -0500

    Update State class to hold up to 128 characters in state name

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,4 +16,4 @@ class State(Base):
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(50), nullable=False)
+    name = Column(String(128), nullable=False)
