f92bd633eb6b5154c5cc70b4cae0f600fad41581
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 05:06:59 +0000

    Add python file to list all states containing 'a' in their name from database using SQLAlchemy; tested locally

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,13 +7,15 @@ from sys import argv
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
         for instance in session.query(State).order_by(State.id):
-                if 'a' in State.name:
-                        print("{}: {}", State.id, State.name)
+                if 'a' in instance.name:
+                        print("{}: {}".format(instance.id, instance.name))
         session.close()
