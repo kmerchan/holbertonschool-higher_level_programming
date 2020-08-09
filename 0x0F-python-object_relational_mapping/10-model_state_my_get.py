9daf11df82ec78320a7ca8f1729edf46d002e41d
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 05:15:37 +0000

    Add python file to print state id matching the state name passed in as an argument to the program using SQLAlchemy; initial commit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,14 +7,17 @@ from sys import argv
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)
-        for instance in session.query(State).filter_by(State.name == argv[4]):
-                print("{}", State.id)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        instance = session.query(State).filter(State.name==argv[4])
+        if instance is not None:
+                print("{}".format(instance.id))
         else:
                 print("Not found")
         session.close()
