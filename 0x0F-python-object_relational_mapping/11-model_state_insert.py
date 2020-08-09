d3e47280f9e365687a3adbfbaba5c169f1ee760e
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 05:31:31 +0000

    Add python file to insert new state 'Louisiana' to table 'states' using SQLAlchemy; tested locally

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -7,12 +7,16 @@ from sys import argv
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
         session.add(State(name='Louisiana'))
         session.commit()
+        for instance in session.query(State).filter_by(name='Louisiana'):
+                print(instance.id)
         session.close()
