a235156f9315944aff39b6985e10db7abb2b9f84
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 05:43:10 +0000

    Add python file to update state with id = 2 to name = New Mexico in database using SQLAlchemy; tested locally

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,13 +7,15 @@ from sys import argv
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)
-        update_state = session.query(State).filter_by(id=2)
-        update_state.name = 'New Mexico'
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for instance in session.query(State).filter_by(id=2):
+                instance.name = 'New Mexico'
         session.commit()
         session.close()
