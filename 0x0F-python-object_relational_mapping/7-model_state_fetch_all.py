508bb0f7a52c3616c591a7bb8792cb5514ac7ccd
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 04:53:02 +0000

    Add python file to display all instances of State from database using SQLAlchemy; tested locally

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,12 +7,14 @@ from sys import argv
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
         Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
         for instance in session.query(State).order_by(State.id):
-                print("{}: {}", State.id, State.name)
+                print("{}: {}".format(instance.id, instance.name))
         session.close()
