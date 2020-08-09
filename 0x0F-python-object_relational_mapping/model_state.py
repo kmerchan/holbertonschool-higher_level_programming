b8fd39ea9e41ec4fec262d33856c0709fd02eafa
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 03:53:29 +0000

    Make executable even if not main for Base and State to be imported by other programs

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,16 +3,16 @@
 defines State class that inherits from Base = declarative_base()
 and links to MySQL table states using SQLAlchemy
 """
-if __name__ == "__main__":
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.ext.declarative import declarative_base
 
-    Base = declarative_base()
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-    class State(Base):
-        """
-        defines State class that links to MySQL table 'states'
-        """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(50), nullable=False)
+Base = declarative_base()
+
+class State(Base):
+    """
+    defines State class that links to MySQL table 'states'
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(50), nullable=False)
