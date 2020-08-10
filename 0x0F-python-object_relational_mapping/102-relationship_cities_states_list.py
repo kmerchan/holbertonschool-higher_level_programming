68efe25b0a7ace5554661386ee38cab709439d05
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 23:26:11 -0500

    Add Python file to list all City objects and corresponding State using state relationship with SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all City objects and corresponding State using states relationship
+using SQLAlchemy and importing State and Base from relationship_state
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for instance in session.query(City).order_by(City.id):
+                print("{}: {}".format(instance.id, instance.name, end=""))
+                print(" -> {}".format(instance.state.name))
+        session.close()
