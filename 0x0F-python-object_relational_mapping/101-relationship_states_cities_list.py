bdd83b616eecfd06940a0086d8f34b3d8b735dc6
Kelsie Merchant <eislek02@gmail.com>
2020-08-09 23:07:52 -0500

    Add python program to print all States and their corresponding cities using the cities relationship and SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all State objects and corresponding Cities using cities relationship
+using SQLAlchemy and importing State and Base from relationship_state
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for instance in session.query(State).order_by(State.id):
+                print("{}: {}".format(State.id, State.name))
+                for inst in session.query(State.cities).order_by(City.id):
+                        print("\t{}: {}".format(City.id, City.name))
+        session.close()
