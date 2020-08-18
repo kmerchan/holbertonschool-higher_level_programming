067f1bbb69be3b1f482a188413d8c15f8dd2b81c
Kelsie Merchant <eislek02@gmail.com>
2020-08-18 17:07:34 -0500

    Update script to assume correct arguments being passed to reduce line count to 3 lines to comply with requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # sends request to given URL and displays the size of the body of response
-if [ $# -lt 1 ]
-then
-    echo "Usage: 0-body_size.sh URL"
-else
-    curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f2
-fi
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f2
