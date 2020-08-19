ad0ed6fd537a45588ece27ff8c1029241f39f857
Kelsie Merchant <eislek02@gmail.com>
2020-08-18 21:33:18 -0500

    Add python function to find peak with smallest time complexity using binary search and text file with time completity in Big O notation

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+defines function to find peak in list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds a peak in a list of unsorted ints
+    """
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    mid = int(len(list_of_integers) / 2)
+    if len(list_of_integers) % 2 == 0:
+        mid = mid - 1
+    middle = list_of_integers[mid]
+    low = list_of_integers[0]
+    high = list_of_integers[-1]
+    # print(list_of_integers)
+    # print("MIDDLE: {}".format(middle))
+    if middle is low and middle is high:
+        return middle
+    if middle is low and middle > list_of_integers[mid + 1]:
+        return middle
+    if middle is high and middle > list_of_integers[mid - 1]:
+        return middle
+    if middle > list_of_integers[mid - 1]:
+        if middle > list_of_integers[mid + 1]:
+            return middle
+        else:
+            peak_high = find_peak(list_of_integers[(mid + 1):])
+            if peak_high is not None:
+                return peak_high
+    else:
+        peak_low = find_peak(list_of_integers[:mid])
+        if peak_low is not None:
+            return peak_low
