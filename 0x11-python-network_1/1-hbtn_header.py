92db75cde9128912b434d3511bbdfd531a999c75
Kelsie Merchant <eislek02@gmail.com>
2020-08-24 14:28:05 -0500

    Update script to not be excuted when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,9 @@
 """
 Python script to send request to given URL and display value of X-Request-Id
 """
-from urllib import request
-from sys import argv
-if argv[1]:
-    with request.urlopen(argv[1]) as response:
-        print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    from urllib import request
+    from sys import argv
+    if argv[1]:
+        with request.urlopen(argv[1]) as response:
+            print(response.getheader('X-Request-Id'))
