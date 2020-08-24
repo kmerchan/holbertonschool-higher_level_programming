9f1fb42b02b978fa40ddbf9bd546d66e5e1c22d9
Kelsie Merchant <eislek02@gmail.com>
2020-08-24 14:26:16 -0500

    Update script to shorten argument check

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@ Python script to send request to given URL and display value of X-Request-Id
 """
 from urllib import request
 from sys import argv
-if argv[1] is not None:
+if argv[1]:
     with request.urlopen(argv[1]) as response:
         print(response.getheader('X-Request-Id'))
