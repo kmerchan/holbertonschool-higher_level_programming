0fcb4f9cfa1fcf426e4e0355b84ebc6b640e2a93
Kelsie Merchant <eislek02@gmail.com>
2020-08-24 14:06:54 -0500

    Add Python script to fetch https://intranet.hbtn.io/status with urllib package to display response type, content, and UTF-8 content of the body of the response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 Python script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request as request
-req = request.Request('https://intranet.hbtn.io/status')
-response = request.urlopen(req)
-print_content = response.read()
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    content = response.read()
+    utf8_content = content.decode("UTF-8")
 print("Body response:")
-print("\t- type: {}".format("don't know"))
-print("\t- content: {}".format(print_content))
-print("\t- utf8 content: {}".format("don't know"))
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
+print("\t- utf8 content: {}".format(utf8_content))
