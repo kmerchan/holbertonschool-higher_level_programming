8f444260a4dbd569888cdbb0b91dab97734d46d1
Kelsie Merchant <eislek02@gmail.com>
2020-08-24 14:29:15 -0500

    Update script to not execute when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,12 @@
 """
 Python script that fetches https://intranet.hbtn.io/status
 """
-import urllib.request as request
-with request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-    utf8_content = content.decode("UTF-8")
-print("Body response:")
-print("\t- type: {}".format(type(content)))
-print("\t- content: {}".format(content))
-print("\t- utf8 content: {}".format(utf8_content))
+if __name__ == "__main__":
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        utf8_content = content.decode("UTF-8")
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
