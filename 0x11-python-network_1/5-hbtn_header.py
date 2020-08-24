1859a60285d3d6dbdbf150680d3636148822af37
Kelsie Merchant <eislek02@gmail.com>
2020-08-24 17:19:11 -0500

    Update script to change syntax since using .get to get specific header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     from sys import argv
     if argv[1]:
         response = requests.get(argv[1])
-        print(response.headers.get['X-Request-Id'])
+        print(response.headers.get('X-Request-Id'))
