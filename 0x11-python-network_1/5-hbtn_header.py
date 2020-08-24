d70999bc254204243a54b7603e44898387c96847
Kelsie Merchant <eislek02@gmail.com>
2020-08-24 17:17:17 -0500

    Update script to use .get to not raise error if header is not in dictionary, instead of try/except

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,8 +5,6 @@ Python script to send request to given URL and display value of X-Request-Id
 if __name__ == "__main__":
     import requests
     from sys import argv
-    try:
+    if argv[1]:
         response = requests.get(argv[1])
-        print(response.headers['X-Request-Id'])
-    except Exception as err:
-        pass
+        print(response.headers.get['X-Request-Id'])
