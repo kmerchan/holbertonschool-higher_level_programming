6fd134a56c51b342fdc0dd150a2e3d37dc1f8701
Kelsie Merchant <eislek02@gmail.com>
2020-08-24 22:33:19 -0500

    Add Python script the takes in repository and owner names and send request to GitHub API to print 10 most recent commits' sha and author

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script to list 10 most recent commits
+to repository 'rails' by user 'rails'
+using GitHub API
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        argv[1], argv[2])
+    try:
+        response = requests.get(url)
+        json_list = response.json()
+        count = 0
+        for obj in json_list:
+            if count == 10:
+                break
+            print("{}: {}".format(
+                obj.get('sha'),
+                obj.get('commit').get('author').get('name')))
+            count = count + 1
+    except Exception as err:
+        print("Error is {}".format(err))
