94e7080ec08e39720a5eaec6fc65755868c82ce0
Kelsie Merchant <eislek02@gmail.com>
2020-09-15 18:45:11 -0500

    Update script to call function recursively instead of using loop

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,9 @@
 const myArgs = process.argv.splice(2);
 const num1 = Number(myArgs[0]);
 function factorial (num) {
-  let result = 1;
-  let i = 0;
-  for (i = num; i > 0; i--) {
-    result *= i;
+  if (isNaN(num) || num < 1) {
+    return (1);
   }
-  return result;
+  return (num * factorial(num - 1));
 }
 console.log(factorial(num1));
