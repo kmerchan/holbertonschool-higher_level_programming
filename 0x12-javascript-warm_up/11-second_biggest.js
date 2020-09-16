410f1b8880b07aec2683d7ab6440df2ba18d9fd0
Kelsie Merchant <eislek02@gmail.com>
2020-09-15 19:03:12 -0500

    Update script to set result to second largest integer between first two arguments, then loop through to update, which catches edge case where the first argument is the largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,16 @@ if (myArgs.length === 0 || myArgs.length === 1) {
   console.log(0);
 } else {
   let i = 0;
-  let max = Number(myArgs[0]);
-  let result = Number(myArgs[0]);
-  for (i = 0; i < myArgs.length; i++) {
+  let max = 0;
+  let result = 0;
+  if (Number(myArgs[0]) > Number(myArgs[1])) {
+    max = Number(myArgs[0]);
+    result = Number(myArgs[1]);
+  } else {
+    max = Number(myArgs[1]);
+    result = Number(myArgs[0]);
+  }
+  for (i = 2; i < myArgs.length; i++) {
     if (Number(myArgs[i]) > max) {
       result = max;
       max = Number(myArgs[i]);
