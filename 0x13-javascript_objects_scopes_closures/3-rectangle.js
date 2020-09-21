108979158b184010b5faa500ffe7d8970007acbc
Kelsie Merchant <eislek02@gmail.com>
2020-09-21 16:29:18 -0500

    Add Javascript file that creates and exports Rectangle class with new method print() to print the rectangle using the character 'X'; add test file to test new code

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  //Method to print the rectangle using 'X'
+  print () {
+    let row = '';
+    let i = 0;
+    for (i = 0; i < this.width; i++)
+      row += 'X'
+    for (i = 0; i < this.height; i++)
+      console.log(row);
+  }
+}
+module.exports = Rectangle;
