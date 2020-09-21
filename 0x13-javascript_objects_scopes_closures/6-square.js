4df07e6e7ec071e477bf267dbc12203abb7e45ee
Kelsie Merchant <eislek02@gmail.com>
2020-09-21 17:18:16 -0500

    Update code to import from 5-square.js and remove unnecessary constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const prevSquare = require('./5-square');
+class Square extends prevSquare {
   // Method to print Square instance using given character or 'X'
   charPrint (c = 'X') {
     let row = '';
