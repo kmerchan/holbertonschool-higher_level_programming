3e51e18ca65cccd899d67b4c11491066472d80d5
Kelsie Merchant <eislek02@gmail.com>
2020-09-21 16:35:16 -0500

    Update code to follow style guidelines

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,14 +6,17 @@ class Rectangle {
       this.height = h;
     }
   }
-  //Method to print the rectangle using 'X'
+
+  // Method to print the rectangle using 'X'
   print () {
     let row = '';
     let i = 0;
-    for (i = 0; i < this.width; i++)
-      row += 'X'
-    for (i = 0; i < this.height; i++)
+    for (i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (i = 0; i < this.height; i++) {
       console.log(row);
+    }
   }
 }
 module.exports = Rectangle;
