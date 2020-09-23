27c2da02c9389b7c4ee4103b1c137360022c4326
Kelsie Merchant <eislek02@gmail.com>
2020-09-22 23:45:26 -0500

    Add Javascript script that uses Star Wars API request to return count of the number of films Wedge Antilles appears in, using his character id: 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,21 @@
 // script that counts the number of films Wedge Antilles is in
 const request = require('request');
 const myArgs = process.argv.slice(2);
-const URLstring = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
-request(URLstring, function (err, response, body) {
+let result = 0;
+request(myArgs[0], function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    console.log(JSON.parse(body).title);
+    const results = JSON.parse(body).results;
+    let i = 0;
+    for (i = 0; i < results.length; i++) {
+      let j = 0;
+      let character = results[i].characters;
+      for (j = 0; j < character.length; j++) {
+	if (character[j].includes('18'))
+	  result += 1;
+      }
+    }
+    console.log(result);
   }
 });
