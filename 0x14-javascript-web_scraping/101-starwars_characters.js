e21131113c785b8bb6149afe037a23ccda222a23
Kelsie Merchant <eislek02@gmail.com>
2020-09-23 14:29:00 -0500

    Add recursive function to call character URL in order of API characters list

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,21 +3,26 @@
 const request = require('request');
 const myArgs = process.argv.slice(2);
 const URLstring = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+function characterFunc (character, i = 0, stop) {
+  if (i === stop) {
+    return;
+  }
+  const URLcharacter = character[i];
+  request(URLcharacter, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+    }
+  });
+  i++;
+  characterFunc(character, i, stop);
+}
 request(URLstring, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
     const character = JSON.parse(body).characters;
-    let i = 0;
-    for (i = 0; i < character.length; i++) {
-      const URLcharacter = character[i];
-      request(URLcharacter, function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    characterFunc(character, 0, character.length);
   }
 });
