29248e2a69173beb645c6d4f45c067cb496186d3
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 13:42:45 -0500

    initial commit

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // script to update the text color of the HTMLtag header to red
+// using document.querySelector
 const header = document.querySelectorAll('header')
 header.forEach(function () {
   style="color: red;"
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// script to update the text color of the HTMLtag header to red
+// using jQuery API
+$('header').css({'color': 'red'})
