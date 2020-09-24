6521c5a19d2903b2f6b4148304f1e85baab5e036
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 17:50:52 -0500

    Add script to be referenced by HTML file that changes the color of the header text to red; add html file to test javascript code

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// script to update the text color of the HTMLtag header to red
+// script that updates the text color of the HTMLtag header to red
 // using jQuery API
 $('header').css({'color': 'red'})
