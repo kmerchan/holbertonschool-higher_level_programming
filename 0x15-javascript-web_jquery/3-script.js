4c66793ef6261e7d6d483173021eaae6d03df5d1
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 18:31:33 -0500

    Add script to be referenced by HTML file that adds the class red to the header tag when the user clicks on the DIV#red_header tag; add html file to test javascript code

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that adds the class red to the HTML header tag
+// when the user clicks on the tag DIV#red_header
+// using jQuery API
+$('DIV#red_header').click(function () {
+  $('header').addClass('red');
+});
