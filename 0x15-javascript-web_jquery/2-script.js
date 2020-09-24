3380b4016cb30a3d26e579fe109323623b65cc90
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 18:11:16 -0500

    Add script to be referenced by HTML file that changes the color of the header text to red when the user clicks on the DIV#red_header tag; add html file to test javascript code

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that updates the text color of the HTML tag header to red
+// when the user clicks on the tag DIV#red_header
+// using jQuery API
+$('DIV#red_header').click(function () {
+  $('header').css({ color: 'red' });
+});
