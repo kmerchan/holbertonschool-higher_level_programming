ce930b62b680265d815a67b4ede2c45ed60b66a7
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 22:10:53 -0500

    Update script to ensure document is fully loaded and ready before running since script is imported in head

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,8 +1,10 @@
 // script that fetches the value of hello from French API URL
 // URL: https://fourtonfish.com/hellosalut/?lang=fr
 // using jQuery API
-const URL = 'https://fourtonfish.com/hellosalut/?lang=fr';
-$.getJSON(URL, function (data) {
-  const hello = data.hello;
-  $('DIV#hello').text(hello);
+$(document).ready(function () {
+  const URL = 'https://fourtonfish.com/hellosalut/?lang=fr';
+  $.getJSON(URL, function (data) {
+    const hello = data.hello;
+    $('DIV#hello').text(hello);
+  });
 });
