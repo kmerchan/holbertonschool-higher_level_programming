b0789316536a48bc74430a8b3d947f366d3e47d7
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 19:54:26 -0500

    Add script to be referenced by HTML file that toggles the class between red and green for the header tag when the user clicks on the DIV#toggle_header tag; add html file to test javascript code

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that toggles the class of the HTML header tag
+// when the user clicks on the tag DIV#toggle_header
+// using jQuery API
+$('DIV#toggle_header').click(function () {
+  if ($('header').hasClass('red')) {
+    $('header').toggleClass('green');
+  } else {
+    $('header').toggleClass('red');
+  }
+});
