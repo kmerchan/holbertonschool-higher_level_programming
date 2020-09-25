9b8445ba84f11d53c8db143fec64f6f3e32b3e8f
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 20:09:24 -0500

    Add script to be referenced by HTML file that changes the text of the header tag when the user clicks on the DIV#update_header tag; add html file to test javascript code

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that changes the text of HTML header tag to "New Header!!!"
+// when the user clicks on the tag DIV#update_header
+// using jQuery API
+$('DIV#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
