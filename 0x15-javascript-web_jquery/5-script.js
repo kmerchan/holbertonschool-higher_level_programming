c53496e7c0fc49d77f716c1593c67b6770407625
Kelsie Merchant <eislek02@gmail.com>
2020-09-24 20:03:06 -0500

    Add script to be referenced by HTML file that adds an item to list UL.my_list tag when the user clicks on the DIV#add_item tag; add html file to test javascript code

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that adds a LI element to a list
+// when the user clicks on the tag DIV#add_item
+// using jQuery API
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+});
