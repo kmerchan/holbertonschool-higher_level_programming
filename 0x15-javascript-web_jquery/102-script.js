ff6ef4132b55f9cd44b7be4e06e529faf41fa010
Kelsie Merchant <eislek02@gmail.com>
2020-09-27 21:08:53 -0500

    Add Javascript script that fetches and prints the value of hello based on the input language code from a language API; add test html file to test code

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+// script that fetches the value of hello from language API URL
+// URL: https://fourtonfish.com/hellosalut/?lang=
+// where the language is the value of the tag INPUT#language_code
+// using jQuery API
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const URL = 'https://www.fourtonfish.com/hellosalut/?lang='
+	  + $('INPUT#language_code').val();
+    $.getJSON(URL, function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
